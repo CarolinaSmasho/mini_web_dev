4df612420be8ab73e0a22dabc34f6b96608737bc
Tawan67 <tawankhuntha@gmail.com>
2026-02-13 21:07:41 +0700

    no cache tracking

--- GamerLFG/obj/Debug/net10.0/GamerLFG.AssemblyInfo.cs
+++ GamerLFG/obj/Debug/net10.0/GamerLFG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamerLFG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61012a0efe275abc66fd40b510aeb41e810c0a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c69d55736a1f6c6ac1a3836f399416d1772c576")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamerLFG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamerLFG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
