8d8cf1a9e9408a7614bccb5256ece906454d65df
xldn <tewly2912@gmail.com>
2026-02-22 21:19:21 +0700

    update profiles css

--- GamerLFG/Controllers/UserController.cs
+++ GamerLFG/Controllers/UserController.cs
@@ -18,6 +18,11 @@ public class UserController : Controller
         return View();
     }
 
+    public IActionResult Profiles()
+    {
+        return View();
+    }
+
 
 // Mock Data: ข้อมูลจำลอง (ในของจริงจะมาจาก DB)
 private readonly List<dynamic> _mockUsers = new List<dynamic>
