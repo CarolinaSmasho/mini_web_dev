675c928e970d6e123591ff35a9603ad56222ed27
Tawan67 <tawankhuntha@gmail.com>
2026-02-22 20:59:21 +0700

    get 10 Lobby per time

--- GamerLFG/Services/Interface/ILobbyService.cs
+++ GamerLFG/Services/Interface/ILobbyService.cs
@@ -4,10 +4,12 @@ namespace GamerLFG.Services.Interface
 {
     public interface ILobbyService
     {
-        Task<LobbyListResponse> GetAllLobbyAsync(string userId);
+        Task<LobbyListResponse> GetAllLobbyAsync(string? userId);
+        Task<List<ShowLobbyDTO>> GetNextLobbiesAsync(string? lastId, int pageSize = 10,string? userId);
         Task CreateLobbyAsync(CreateLobbyDTO newLobby);
         Task DeleteLobbyAsync (string id);
         Task UpdateLobbyAsync (Lobby lobby);
         Task AddmemberAsync (Lobby current_lobby,User newUser);
+        
     }
 }
--- GamerLFG/Services/LobbyService.cs
+++ GamerLFG/Services/LobbyService.cs
@@ -11,13 +11,13 @@ namespace GamerLFG.Services
         private readonly IMongoCollection<User> _users;
         public LobbyService(IMongoDatabase database)
         {
-            _lobbies = database.GetCollection<Lobby>("Lobby");
+            _lobbies = database.GetCollection<Lobby>("Lobby"); //ดึง collection lobby มาเก็บไว้เป็นตัวแปร
         }
-        public async Task<LobbyListResponse> GetAllLobbyAsync(string userId){
+        public async Task<LobbyListResponse> GetAllLobbyAsync(string? userId = null){ // ใช้function แบบ async
             
-            var lobbyList = await _lobbies.Find(_ => true).ToListAsync();
-            var separatedLobbies = lobbyList.ToLookup(lob => lob.HostId == userId);
-            var myLobby = separatedLobbies[true].Select( lob => new ShowLobbyDTO{
+            var myLobbyList = await _lobbies.Find(mylobby => mylobby.HostId == userId).ToListAsync(); // ดึง lobby ของเรามาแบบ async
+            var otherLobbyList = await _lobbies.Find(otherLobby => otherLobby.HostId != userId).Limit(10).ToListAsync();
+            var myLobby = myLobbyList.Select( lob => new ShowLobbyDTO{
                 Id = lob.Id,
                 Title  = lob.Title,
                 Game = lob.Game,
@@ -29,7 +29,7 @@ namespace GamerLFG.Services
                 MaxPlayers = lob.MaxPlayers
                 
             }).ToList();
-            var publicLobby = separatedLobbies[false].Select( lob => new ShowLobbyDTO{
+            var publicLobby = otherLobbyList.Select( lob => new ShowLobbyDTO{
                 Id = lob.Id,
                 Title  = lob.Title,
                 Game = lob.Game,
@@ -48,11 +48,39 @@ namespace GamerLFG.Services
                 OtherLobbies = publicLobby
             };
         
-
             }
             
             
-        
+        public async Task<List<ShowLobbyDTO>> GetNextLobbiesAsync(string? lastId,string? userId, int pageSize = 10)
+            {
+                // ใช้ .Ne() แทน .Eq()
+                var filter = Builders<Lobby>.Filter.Ne(l => l.HostId, userId);
+
+                
+                // กรองเอาเฉพาะตัวที่ ID "หลังจาก" ตัวสุดท้ายที่หน้าจอแสดงอยู่
+                filter = Builders<Lobby>.Filter.Gt(l => l.Id, lastId);
+                var nextLobby = await _lobbies.Find(filter)
+                                .SortBy(l => l.Id) // ต้องเรียงลำดับเสมอเพื่อให้ข้อมูลต่อเนื่องกัน
+                                .Limit(pageSize)
+                                .ToListAsync();
+                
+                
+
+                return nextLobby.Select( lob => new ShowLobbyDTO{
+                    Id = lob.Id,
+                    Title  = lob.Title,
+                    Game = lob.Game,
+                    Description = lob.Description,
+                    HostName  = lob.HostName,
+                    Picture = lob.Picture,
+                    Moods = lob.Moods,
+                    Currentplayers = lob.Members.Count,
+                    MaxPlayers = lob.MaxPlayers
+                    
+                    }).ToList();
+                }
+
+                
         public async Task CreateLobbyAsync(CreateLobbyDTO newLobby){
             
             
