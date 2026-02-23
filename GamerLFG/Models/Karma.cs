26e4675fa35f049c43ffd115db5892884cc87914
xldn <tewly2912@gmail.com>
2026-02-23 17:28:20 +0700

    Update Karma.cs

--- GamerLFG/Models/Karma.cs
+++ GamerLFG/Models/Karma.cs
@@ -1,18 +1,22 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-public class KarmaHistory
+
+namespace GamerLFG.Models 
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public class KarmaHistory
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string TargetUserId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string TargetUserId { get; set; }
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string FromUserId { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FromUserId { get; set; }
 
-    public double Score { get; set; }
-    public string Comment { get; set; }
-    public DateTime Date { get; set; } = DateTime.UtcNow;
+        public double Score { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+    }
 }
